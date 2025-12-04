9581eff91033db5625938aa7fe4c22a1a25276ee
hanasbp <hanasoke@gmail.com>
2025-12-04 22:31:35 +0700

    added the others routes in main.go

--- main.go
+++ main.go
@@ -13,6 +13,12 @@ func main() {
 	// 1.Homepage
 	http.HandleFunc("/", homecontroller.Welcome)
 
+	// 2.Categories
+	http.HandleFunc("/categories", categorycontroller.Index)
+	http.HandleFunc("/categories/add", categorycontroller.Add)
+	http.HandleFunc("/categories/edit", categorycontroller.Edit)
+	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
