629ef3b3d291ea891be1bc950ba0100486cb5bed
hanasbp <hanasoke@gmail.com>
2025-12-05 22:19:33 +0700

    added productcontroller page

---
+++ controllers/productcontroller/productcontroller.go
@@ -0,0 +1,23 @@
+package productcontroller
+
+import "net/http"
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Detail(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+}
